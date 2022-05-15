2d9a8fcfa350d83ea6b2c60eba9782ceb440d6ee
Heo Eun <64454313+DAASHeo@users.noreply.github.com>
2022-05-15 23:58:33 +0900

    볼링게임 - 점수 카운트 기능 추가, 점수 카운트 기준 수정
    점수 표기하는 과정에서 rotation의 값을 기준으로 점수를 카운트하면 무한으로 점수가 증가하게 되는 문제점으로 인해
    1. 볼링공과 충돌해서 rotation값이 넘어지는 값이 될 경우
    2. 다른 핀들과 충돌해서 바닥으로 아예 떨어졌을 경우(바닥과의 충돌)
    나누어서 점수를 카운트하도록 코드를 작성
    
    Issue >
    1. 볼링핀이 스쳐 지나가서 안 넘어졌는데 카운트가 되는 현상 (-> Debug.Log를 통해 transform.eulerangles 확인한 결과, 쓰러지지 않고 스쳐 지나갔지만 z 회전축의 값이 359.9로 찍혀서 조건문을 실행하는 이상한 현상이 발생)
    2. 볼링핀들이 서로 충돌해서 밑에 있는 바닥이 아닌 floor에 쓰러지게 되는 경우 카운트가 안 됨
    이러한 특수한 경우가 아닌 이상 문제없이 잘 작동

--- 볼링게임/ballControl.cs
+++ 볼링게임/ballControl.cs
@@ -14,18 +14,23 @@ public class ballControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        //if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             print("Grab!");
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero;
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
             Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             transform.position = objPosition;
         }
         if(Input.GetMouseButtonUp(0))
         {
+            Debug.Log("Throw!");
             rb = GetComponent<Rigidbody>();
             rb.useGravity = true;
-            rb.AddForce(0, 0, 30.0f, ForceMode.Impulse);
+            rb.AddForce(0, 0, 50.0f, ForceMode.Impulse);
         }
     }
+
 }
